93e925fe0c4601106a9342d0ae30ce30f1b57d2d
Lavanya <lavanyaj@cs.stanford.edu>
2018-01-27 21:00:00 -0800

    updated print_links for 144 server fct topo

--- print_links.py
+++ print_links.py
@@ -1,6 +1,7 @@
-end=32
-link_rate=90
+end=144
+link_rate=100
 num_tors = int(round(end/16))
+num_aggs= 4
 for i in range(0, end):
     tor = int(i/16) + 144
     print "%d %d %f" % (i, tor, link_rate)
@@ -8,7 +9,7 @@ for i in range(0, end):
     pass
 
 for tor in range(144, (144+num_tors)):
-    for agg in range(153, 160):
+    for agg in range(153, 153+num_aggs):
         print "%d %d %f" % (tor, agg, 4*link_rate)
         print "%d %d %f" % (agg, tor, 4*link_rate)
         pass
